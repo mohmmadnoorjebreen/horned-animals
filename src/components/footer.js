f34af38f8b6faf104e5c69e1f483091e6435de21
mohmmadnoorjebreen <mohammadnoormjk1998@gmail.com>
2021-06-06 23:07:42 +0300

    301-lab01

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from 'react'; 
+
+
+class Footer extends React.Component {
+
+    render() { 
+        return (
+            <footer>
+                <p> by mohmmad noor jebreen</p>
+            </footer>
+        )
+    }
+}
+
+
+export default Footer;
