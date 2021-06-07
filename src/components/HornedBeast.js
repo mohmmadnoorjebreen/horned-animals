c944774381c0d657b9a7b7ac960652f74ac2336e
mohmmadnoorjebreen <mohammadnoormjk1998@gmail.com>
2021-06-07 19:01:20 +0300

    lab02

---
+++ src/components/HornedBeast.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+
+
+class HornedBeast extends React.Component {
+
+    constructor(props) {
+       
+        super(props)
+
+      
+        this.state = {
+            title: this.props.title,
+            image: this.props.image,
+            description: this.props.description,
+            star : 0
+        }
+    }
+
+  
+    click = () => {
+        let newCount = this.state.star;
+        this.setState({
+            star: newCount += 1
+        })
+    }  
+    
+
+    render() {
+
+        return (
+        <div>
+            <h1>
+            {this.state.title}
+            </h1>
+            <img onClick={this.click} src={this.state.image} alt={this.state.title} />
+            
+            <p>
+                {this.state.description}
+            </p>
+           <span>
+           &#9733;  {this.state.star} 
+           </span>
+
+        </div>
+        )
+    }
+}
+
+
+export default HornedBeast;
